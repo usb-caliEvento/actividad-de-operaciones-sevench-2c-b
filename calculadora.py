2b9351bdc782a911c520360a95560bf791179a10
ElVic11 <danielvictoriafranco@gmail.com>
2025-10-27 10:59:57 -0500

    Operaciones

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+def calculadora():
+    a = input ("Primer numero: ")
+    b = input ("Segundo numero ")
+    suma = a + b
+    resta = a - b
+    multiplicacion = a * b
+    division = a / b
+    print (" La suma de ", a, " + ", b, " = ", suma)
+    print (" La resta de ", a, " - ", b, " = ", resta) 
+    print (" La multiplicacion de ", a, " * ", b, " = ", multiplicacion)
+    print (" La division de ", a, " / ", b, " = ", division)
+    
+calculadora()
